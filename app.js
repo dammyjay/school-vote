1a991fb8553d9bbc3ca2ab8a1d312b37179d1846
Dammyjay <dammykirchhoff@gmail.com>
2025-10-27 13:33:25 +0100

    views renamed app js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("Views", path.join(__dirname, "Views"));
 
 // Session
 app.use(

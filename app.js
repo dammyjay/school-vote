b658816f10b19e271ad5347e58759a6fcf9d7618
Dammyjay <dammykirchhoff@gmail.com>
2025-10-27 13:15:58 +0100

    app js and json updated

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express();
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
